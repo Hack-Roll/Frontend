ee3f872503de059608c10d022b797b31bd1c883a
kat-lev <kat@katleverton.com>
2025-05-07 14:54:11 +0200

    Add Logout button to Navbar and delete JWT on Logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import WhiteLogo from "../../assets/WhiteLogo.png";
+import Button from '../button/button';
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/");
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarLogo}>
@@ -15,7 +30,11 @@ const Navbar = () => {
         <li><NavLink to="/eventList" className={({ isActive }) => isActive ? styles.active : ''}>Events</NavLink></li>
         <li><NavLink to="/addEvents" className={({ isActive }) => isActive ? styles.active : ''}>Add Events</NavLink></li>
         <li><NavLink to="/userProfile" className={({ isActive }) => isActive ? styles.active : ''}>Profile</NavLink></li>
-        <li><NavLink to="/loginRegister" className={({ isActive }) => isActive ? styles.active : ''}>Login</NavLink></li>
+        <li>
+          {isLoggedIn ? (<Button text="Logout" onClick={handleLogout} />
+            ) : (
+          <NavLink to="/loginRegister"><Button text="Login" /></NavLink>)}
+        </li>
       </ul>
     </nav>
   );
