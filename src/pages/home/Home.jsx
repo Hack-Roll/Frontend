1e06bc1bf350d0b3b6f6061541abd450339aae43
Israel Espin <israel.espin92@gmail.com>
2025-05-07 11:10:10 +0200

    added text to page home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,8 +5,12 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Bienvenido a la página de inicio</h1>
-      <p>Explora los eventos y más desde aquí.</p>
+      <h1>¡Bienvenide a Hack n' Roll!</h1>
+      <p>
+La plataforma donde los hackers, makers y soñadores se reúnen para construir el futuro.
+En Hack n' Roll, organizamos y compartimos hackatones, talleres y eventos tech que impulsan la innovación y la colaboración. Ya sea online o presencial, aquí es donde tus ideas cobran vida junto a una comunidad apasionada por la tecnología.
+¿Estás list@ para crear algo increíble?
+🔥 Explora eventos, únete a la comunidad y prepárate para hackear el mundo.</p>
     </div>
   );
 };
